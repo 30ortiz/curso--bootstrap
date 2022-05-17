b85312ec8d5b36a8f28ebf961b466641c00e6c38
It's a me LuisMario <lmarioortiz@outlook.com>
2022-05-17 16:02:31 -0500

    Mandar a produccion

---
+++ components/js/index.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+$(document).ready(function () {
+  $('body').scrollspy({
+    target: "#navbar-example",
+    offset:57
+  });
+});
